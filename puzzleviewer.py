88b7cde08f959b8b03ad00325cd5f419a573e6c5
Joeri Jurion <jurion@septentrio.com>
2021-09-14 14:47:15 +0200

    Insert numbers with assertions

--- puzzleviewer.py
+++ puzzleviewer.py
@@ -3,20 +3,37 @@ import numpy as np
 
 
 class PuzzleViewer:
-  def __init__(self, dim):
+
+  """
+  Initialize puzzle viewer by creating white screen with given dimensions and fills in numbers given by optional 2D array.
+  """
+  def __init__(self, dim, matrix=0):
+    if matrix == 0:
+      self.matrix =   [
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+      ]
+    else:
+      self.matrix = matrix
     self.img = np.zeros((dim,dim, 3), dtype='uint8')
     cv.rectangle(self.img, (0,0), (self.img.shape[0],self.img.shape[1]), (255,255,255), thickness=cv.FILLED) ;#Fill blank screen white
     self.drawGrid()
-    self.addNumber(0, 1, 1)
-    self.addNumber(1, 5, 1)
-    self.addNumber(2, 2, 1)
-    self.addNumber(3, 3, 1)
-    self.addNumber(4, 4, 1)
-    self.addNumber(5, 1, 3)
-    self.addNumber(6, 1, 5)
-    self.addNumber(7, 1, 4)
+    for i in range(len(matrix)):
+      for j in range(len(matrix[i])):
+        self.insertNumber(matrix[j][i], i, j, True)
+    self.insertNumber(9, 1, 4)
 
 
+  """
+  Draws the sudoku grid on the image.
+  """
   def drawGrid(self):
     for i in range(10):
       if i in {0, 3, 6, 9}:
@@ -26,14 +43,44 @@ class PuzzleViewer:
       cv.line(self.img, (self.img.shape[0]//9*i,0), (self.img.shape[0]//9*i, self.img.shape[1]), (0,0,0), thickness=thickness) ;#Vertical lines
       cv.line(self.img, (0,self.img.shape[1]//9*i), (self.img.shape[0],self.img.shape[1]//9*i), (0,0,0), thickness=thickness) ;#Horizontal lines
 
-  def addNumber(self, value, cellX, cellY):
-    x = self.img.shape[0]//9*cellX + self.img.shape[0]//32
-    y = self.img.shape[1]//9*cellY + self.img.shape[0]//18 + self.img.shape[0]//32
-    cv.putText(self.img, str(value), (x,y), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0,0,0), 2)
+
+  """
+  Inserts a number with a specified value, x- and y coordinates. 
+  If the viewer is initialized the numbers are printed in black, otherwise in red.
+  """
+  def insertNumber(self, value, cellX, cellY, init=False):
+    color = (0,0,0)
+    if not init:
+      assert self.matrix[cellY][cellX] == 0, f"The cell is not empty at coordinates ({cellX}, {cellY}), it's value is already {self.matrix[cellY][cellX]}."
+      color = (0,0,255)
+    assert value in range(10), f"The value of inserted number should be [0-9], value is {value}."
+    
+    if value != 0:
+      x = self.img.shape[0]//9*cellX + self.img.shape[0]//32 + self.img.shape[0]//160 ;#move to cell + move a quarter of a cell to right + move a fraction more to right
+      y = self.img.shape[1]//9*cellY + self.img.shape[0]//18 + self.img.shape[0]//40  ;#move to cell + move half a cell downward + move a fraction more downwards
+      cv.putText(self.img, str(value), (x,y), cv.FONT_HERSHEY_SCRIPT_SIMPLEX  , 1.0, color, 2)
     
 
 
-viewer = PuzzleViewer(495)
-cv.imshow('puzzle', viewer.img)
 
-cv.waitKey(0)
+
+"""
+Test scripts
+"""
+if __name__ == "__main__":
+  matrix = [
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,1,0,0,0,9,0,0],
+    [0,0,0,2,0,0,0,8,0],
+    [0,0,0,0,3,0,0,0,7],
+    [0,0,0,0,0,4,0,6,0],
+    [0,0,0,0,0,0,5,0,0],
+  ]
+
+  viewer = PuzzleViewer(495, matrix)
+  cv.imshow('puzzle', viewer.img)
+
+  cv.waitKey(0)
